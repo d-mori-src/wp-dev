351ab6dff88a992936d363e1cbb01863ef4ba428
p000078 <p000078@cpe-172-100-100-13.twcny.res.rr.com>
2023-04-25 13:31:03 +0900

    230425アイキャッチ画像埋め込み追加

--- single.php
+++ single.php
@@ -5,6 +5,7 @@
 
 <?php get_header(); ?>
     <h1><?php the_title(); ?></h1>
+    <?php the_post_thumbnail('full');?> 
     <?php the_content(); ?>
     <a href="<?=$site_url?>">← ホームへ</a>
 <?php get_footer(); ?>
